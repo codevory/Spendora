2b53e7ead9931868bf0e9282ecaf282e092c4cb0
codevory <shahijahan.codes@gmail.com>
2026-03-25 17:33:58 +0530

    improved page ui design

--- src/pages/AddExpense.tsx
+++ src/pages/AddExpense.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react'
+import { HiOutlineMenu } from "react-icons/hi"
 import AddTransactionForm from "../components/AddTransactionForm"
-import { deleteTransaction } from "../store/features/transaction"
-import { useAppDispatch, useAppSelector } from "../store/store"
+import RecentTransactions from "../components/RecentTransactions"
+import SidebarMenu from "../components/SidebarMenu"
+
 
 const AddExpense = () => {
-    const data = useAppSelector((state) => state.transaction.transactions)
-    const dispatch = useAppDispatch()
+  const [isOpen,setIsOpen] = useState<boolean>(true)
+
   return (
-    <div className="flex-col gap-20 bg-sky-900 flex justify-center items-center h-dvh">
-      <AddTransactionForm />
-      <div className="bg-gray-700">
-        <ul className="flex flex-col gap-2">
-       {data.map((tx) => <li key={tx.transactionId} className="flex gap-3 "><span className="flex gap-3">{tx.amount} {tx.name}</span>
-       <button className=" w-20 h-10 flex justify-center items-center bg-red-700 text-white font-medium rounded-xl cursor-pointer" onClick={() => dispatch(deleteTransaction(tx.transactionId))}>Delete</button>
-       </li>
-    )}
-        </ul>
+    <div className=" bg-sky-900 flex flex-col p-2 overflow-hidden">
+      <div className="bg-red-400 w-full h-20 flex items-center px-2">
+        <HiOutlineMenu onClick={() => setIsOpen((p) => !p)} startOffset={2} stroke="blue" className="h-10 w-10 cursor-pointer active:scale-95" />
       </div>
+
+      <div className="w-full h-dvh flex gap-5">
+
+      {/* //left side */}
+      <div className={` ${isOpen ? 'w-[17%] transform-content' : 'w-[6%] transform-content -transate-x-20' } h-full bg-green-900`}>
+      <SidebarMenu isOpen={isOpen} />
+      </div> 
+
+      <div className="w-full">  
+      <AddTransactionForm />
+      <div className="bg-gray-400">
+      <RecentTransactions />
+      </div>
+      </div>
+      {/* //right side */}
+
+      <div className="bg-purple-700 w-1/3">
+        <h2> recent transactions </h2>
+        <div>
+            <RecentTransactions />
+        </div>
+      </div>
+      </div>
+
     </div>
   )
 }
