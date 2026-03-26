56ada13b740d749c20feba313fb1d71af9c67d43
codevory <shahijahan.codes@gmail.com>
2026-03-26 16:14:28 +0530

    improved ui for recent tnx

--- src/components/RecentTransactions.tsx
+++ src/components/RecentTransactions.tsx
@@ -2,14 +2,37 @@ import { useAppSelector } from "../store/store"
 
 const RecentTransactions = () => {
 const data = useAppSelector((state) => state.transaction.transactions)
-    
-  return (
-    <div>
-      {data.map((item) => (
-        <span key={item.transactionId} className="flex gap-3"><b>${item.amount}</b> | <p>{'Tue'}</p> | {<b>{item.name}</b>}</span>
-      ))}
+
+
+return (
+  <div className="card mt-6">
+  <h2 className="text-lg font-semibold mb-4">Recent Transactions</h2>
+
+  {data.map((item) => (
+    <div className="transaction-item border-b border-slate-700 last:border-none">
+      
+      <div>
+        <p className="font-medium">{item.name}</p>
+        <p className="text-sm text-muted">
+          {item.date} • {getDayName(item.date)}
+        </p>
+      </div>
+
+      <p className="text-red-400 font-semibold">
+        -₹{item.amount}
+      </p>
+
     </div>
-  )
+  ))}
+</div>
+)
+
 }
 
 export default RecentTransactions
+
+function getDayName(dateString:string){
+  return new Date(dateString).toLocaleDateString("en-us",{
+    weekday:"short",
+  })
+}
