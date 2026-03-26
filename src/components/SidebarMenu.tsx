44d3ffe23ab80d3f1011c82816e3ebc8e9078623
codevory <shahijahan.codes@gmail.com>
2026-03-26 16:15:29 +0530

    add menu items with change in toggle

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -2,16 +2,14 @@ import { TiHomeOutline } from "react-icons/ti";
 
 const SidebarMenu = ({isOpen}:{isOpen:boolean}) => {
   return (
-    <div className='flex flex-col gap-3 justify-center items-center bg-blue-500'>
-      <span>{`${isOpen ? 'Add expense' :  <TiHomeOutline /> }`}</span>
-      <span>menu 2</span>
-      <TiHomeOutline />
-      <span>menu 3</span>
-      <span>menu 4</span>
-       <span>menu 5</span>
-       <span>menu 6</span>
-       <span>menu 7</span>
-       <span>menu 8</span>
+    <div className='flex flex-col gap-10 items-center h-full mt-1 font-semibold text-xl sidebar '>
+      <span className="sidebar-item">{isOpen ? '🏠 Dashboard' :  <TiHomeOutline /> }</span>
+      <span className="sidebar-item">{isOpen ?  '💸 Add Expense' : "💸"}</span>
+      <span className="sidebar-item">{ isOpen ? '📊 Analytics' : '📊'}</span>
+      <span className="sidebar-item">{isOpen ? ' ⚠️ Budget Alerts' : '⚠️'}</span>
+      <span className="sidebar-item">{isOpen ?  '🧾 Transactions' : '🧾'}</span>
+      <span className="sidebar-item">{isOpen ? '🏷 Categories' : '🏷'}</span>
+      <span className="sidebar-item">{isOpen ? '⚙️ Settings' : '⚙️'}</span>
     </div>
   )
 }
