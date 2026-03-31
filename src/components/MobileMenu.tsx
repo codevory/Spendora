dc7d4c18a75af807fa836e00ec0efc4f500e7278
codevory <shahijahan.codes@gmail.com>
2026-03-31 21:03:56 +0530

    mobile menu

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MobileMenu = () => {
+  return (
+    <div className='mobile-menu '>
+      <span className='mobile-menu-items active:bottom-10 '>Menu item</span>
+      <span className='mobile-menu-items'>Menu item</span>
+      <span className='mobile-menu-items'>Menu item</span>
+      <span className='mobile-menu-items'>Menu item</span>
+    </div>
+  )
+}
+
+export default MobileMenu
