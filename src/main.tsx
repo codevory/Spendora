a3bf2b621b227bc46b5987aa83cf2b3874f3b14f
codevory <shahijahan.codes@gmail.com>
2026-04-02 22:35:11 +0530

    BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import Store from './store/store.ts'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
     <Provider store={Store}>
     <App />
-    </Provider>
+    </Provider>    
+    </BrowserRouter>
   </StrictMode>,
 )
