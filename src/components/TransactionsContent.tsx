8e0eb6d8a37ca6f66a9fdf6118d663389fe60e2a
codevory <shahijahan.codes@gmail.com>
2026-04-03 19:09:17 +0530

    implement category filter in transactions page

--- src/components/TransactionsContent.tsx
+++ src/components/TransactionsContent.tsx
@@ -1,28 +1,66 @@
 import { useAppSelector } from '../store/store'
 import { Link } from 'react-router-dom';
+import { useState,useMemo } from 'react'
+import type { TransactionType } from '../types/transactionType';
+import EmptyState from './EmptyState';
 
-const TransactionsContent = () => {
+
+interface TransactionsContentPropsType {
+    query : TransactionType['category'] | undefined,
+}
+
+const TransactionsContent = ({query}:TransactionsContentPropsType) => {
+    const [localData,setLocalData] = useState<TransactionType[]>([])
 
     const data = useAppSelector((state) => state.transaction.transactions);
+
+    useMemo(() => {
+        if(query === undefined){
+            setLocalData(data);
+            return
+        }
+    const result = data.filter((txn) => txn.category === query)
+    setLocalData(result)
+    },[query,data])
+   
+
+     const noData =  <div className='transaction-container h-dvh min-h-100 overflow-y-scroll rounded-xl'>
+        <div className=' border-2 rounded px-3 py-1'>
+            <div className='flex justify-between items-center mx-4'>
+                <p className='font-bold m-2'>Id</p>
+                <p className='font-bold m-2'>Transaction id</p>
+                <p className='font-bold m-2'>₹ amount</p>
+            </div>
+            <div className='flex justify-center flex-col gap-1'>
+           <h2 className='font-semibold text-red-500 text-xl'>No data to display</h2>
+           <p className='text-xm text-white'>add expenses to view transactions</p>
+            </div>
+        </div>
+    </div>
+
+    if(!localData.length){
+        return <EmptyState content={noData} />
+    }
+
   return (
-    <div className='transaction-container w-[55%] h-dvh overflow-y-scroll rounded-xl'>
+    <div className='transaction-container  h-dvh overflow-y-scroll rounded-xl'>
         <div className=' border-2 rounded'>
             <div className='flex justify-between items-center mx-4'>
                 <p className='font-bold m-2'>Id</p>
                 <p className='font-bold m-2'>Transaction id</p>
                 <p className='font-bold m-2'>₹ amount</p>
             </div>
-   { data.map((tnx,idx) => (
-       <div key={tnx.transactionId}>
-            <Link to={`/transactions/tnx-details/${tnx.transactionId}`}>
+            {localData.map((txn,idx) => (
+       <div key={txn.transactionId}>
+            <Link to={`/transactions/tnx-details/${txn.transactionId}`}>
             <div className='flex items-center justify-between px-4 py-1'>
             <p className='bg-black text-white p-2 rounded-xl m-2'>{idx + 1}</p>
-            <span className='transaction-item'>{tnx.transactionId.slice(0,12)}</span>
-            <span className='tag tag-entertainment'>₹ {tnx.amount}</span>
+            <span className='transaction-item'>{txn.transactionId.slice(0,12)}</span>
+            <span className='tag tag-entertainment'>₹ {txn.amount}</span>
             </div>
         </Link>
         </div>
-    ))}
+            ))}
         </div>
     </div>
   )
--- src/pages/TransactionLayout.tsx
+++ src/pages/TransactionLayout.tsx
@@ -4,21 +4,34 @@ import TransactionsContent from '../components/TransactionsContent'
 import SidebarMenu from '../components/SidebarMenu'
 import ViewTransactionDetails from '../components/ViewTransactionDetails'
 import TrendGraph from '../charts/TrendGraph'
+import { useAppSelector } from '../store/store'
+import type { TransactionType } from '../types/transactionType'
 
 const TransactionLayout = () => {
   const [isOpen,setIsOpen] = useState<boolean>(true)
+  const [query,setQuery] = useState<TransactionType['category'] | undefined>()
+  const data = useAppSelector((state) => state.transaction.transactions)
+  const categories = [...new Set(data.map((txn) => txn.category))] as TransactionType['category'][]
   return (<div className=''>
       <Layout onToggle={() => setIsOpen((p) => !p)}>
         <div className='flex gap-4 transaction-layout bg-main  '>
            <div className={` ${isOpen ? 'w-[14%] transform-content' : 'w-[6%] transform-content -transate-x-20' } sidebar bg-green-900`}>
             <SidebarMenu isOpen={isOpen} />
            </div>
-           <div className='flex mt-2 justify-center w-screen h-dvh'>
+           <div className=' mt-2 grid grid-cols-2 w-screen h-dvh'>
+            <div className=''>
+              <select value={query ?? ''} onChange={(e) => setQuery(e.target.value ? (e.target.value as TransactionType['category']) : undefined)}>
+              <option value=''>All Categories</option>
+             {categories.map((category) => (
+              <option key={category} value={category}>{category}</option>
+             ))}
+              </select>
+           <TransactionsContent query={query} />
+            </div>
 
-           <TransactionsContent />
 
            <div className='flex flex-col gap-5 items-center w-fit'>
-           <ViewTransactionDetails />
+           <ViewTransactionDetails data={data} />
            <div className='w-full h-full min-w-140 min-h-110'>
             <TrendGraph />
            </div>
