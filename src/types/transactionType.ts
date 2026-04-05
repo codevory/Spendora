314986040943a91246fe9d449d1f148f3675883a
codevory <shahijahan.codes@gmail.com>
2026-04-05 14:39:09 +0530

    add incomeTransactionType interface

--- src/types/transactionType.ts
+++ src/types/transactionType.ts
@@ -6,3 +6,11 @@ export interface TransactionType {
     transactionId:string;
     createdAt : number;
 }
+
+export interface IncomeType {
+    amount:number;
+    source:string;
+    date:string;
+    transactionId:string;
+    createdAt:number;
+}
