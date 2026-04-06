7e47acf8e4fb3378f772f5ee654b558d52dd1bee
codevory <shahijahan.codes@gmail.com>
2026-04-06 16:28:27 +0530

    Fixes : duplicate category adding

--- src/components/AddCategoryForm.tsx
+++ src/components/AddCategoryForm.tsx
@@ -1,27 +1,44 @@
 import toast from "react-hot-toast";
 import { addNewCategory } from "../store/features/transaction";
-import { useAppDispatch } from "../store/store"
+import { useAppDispatch, useAppSelector } from "../store/store"
 import { useState } from 'react'
+import type { CategoryPropsType } from "../types/transactionType";
 
-const AddCategoriesForm = () => {
-     const [category,setCategory] = useState<string>('')
+const AddNewCategoryForm = () => {
+     const [category,setCategory] = useState<string>("")
+     const categories = useAppSelector((state) => state.transaction.categories)
+
     const dispatch = useAppDispatch();
     const success = () => toast.success("added Successfully")
     const failed = (message:string) => toast.error(message)
 
     function handleSubmit(e:React.SubmitEvent<HTMLFormElement>){
         e.preventDefault();
-        if(category === "")return failed("kindly type category name")
+        if(category.trim().length < 3)return failed("kindly type category name");
+        
+        const newCategory:CategoryPropsType = {
+          name:category,
+          id: Date.now().toFixed(6) 
+        }
         try {
-            dispatch(addNewCategory(category))
-            setCategory('')
-            success()
-        } catch (error) {
+          const normalized = category.trim().toLowerCase();
+          const exists = categories.some((c) => {
+            const name = c?.name?.trim().toLowerCase()
+            return name === normalized
+          })
+          if(exists){
+            return failed("😩Category already exists")
+          }
+            dispatch(addNewCategory(newCategory))
+          } catch (error) {
             failed("Failed to add");
             if(error instanceof Error){
-                failed(error.message)
+              failed(error.message)
             }
             console.log(error)
+          }finally{  
+            success()
+            setCategory('')
         }
     }
   return (
@@ -30,7 +47,7 @@ const AddCategoriesForm = () => {
       <form onSubmit={(e) => handleSubmit(e)} className="form flex flex-col gap-2 ">
         <div className=" flex flex-col">
         <label className='text-accent label'>category</label>
-        <input className='input' type="text" value={category} onChange={(e) => setCategory(e.target.value)} />
+        <input className='input' type="text" value={category} onChange={(e) => setCategory(e.target.value)} required />
         </div>
 
         <button className="button w-20 h-11 font-semibold hover:bg-slate-500 active:scale-95 glass" type="submit">Add</button>
@@ -39,4 +56,4 @@ const AddCategoriesForm = () => {
   )
 }
 
-export default AddCategoriesForm
+export default AddNewCategoryForm
