29073bf1bb5b9a15c0f3be442744c3186a9dcc19
codevory <shahijahan.codes@gmail.com>
2026-04-07 10:09:46 +0530

    normalize transactions based on expense & income

--- src/components/RecentTransactions.tsx
+++ src/components/RecentTransactions.tsx
@@ -2,8 +2,30 @@ import { useAppSelector } from "../store/store"
 
 const RecentTransactions = () => {
 const data = useAppSelector((state) => state.transaction.transactions)
+const userIncomeData = useAppSelector((state) => state.incomeTransaction.incomeTransactions)
 
-const copy = [...data]
+const normalizedIncomeData = userIncomeData.map((txn) => {
+  return {
+    amount:txn.amount,
+    transactionId:txn.transactionId,
+    name:txn.source,
+    date:txn.date,
+    createdAt:txn.createdAt,
+    type:'credited'
+  }
+})
+
+const normalizedData = data.map((txn) => {
+ return {
+    amount:txn.amount,
+    transactionId:txn.transactionId,
+    name:txn.name,
+    date:txn.date,
+    createdAt:txn.createdAt,
+    type:'deducted'
+  }
+})
+const copy = [...normalizedData,...normalizedIncomeData]
 return (
   <div className="card mt-6">
   <h2 className="text-lg font-semibold mb-4">Recent Transactions</h2>
@@ -19,9 +41,9 @@ return (
         </p>
       </div>
 
-      <p className="text-red-400 font-semibold">
-        -₹{item.amount}
-      </p>
+      <div className={`${item.type === "deducted" ? 'text-red-400' : 'text-green-400'}`}>
+      <span className="font-semibold flex items-center">{`${item.type === "deducted" ? '-' : '+'}`}₹{item.amount}</span>
+      </div>
 
     </div>
     )
