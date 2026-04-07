a9d6ccb41eaab91bfc11582e61eeba37618fd0e8
codevory <shahijahan.codes@gmail.com>
2026-04-07 19:12:49 +0530

    added monthly unser income & expense data

--- src/Hooks/useUserData.ts
+++ src/Hooks/useUserData.ts
@@ -1,6 +1,6 @@
 import type { ChartData } from "chart.js"
 import { useAppSelector } from "../store/store"
-import type { TransactionType } from "../types/transactionType";
+import type { IncomeType, TransactionType } from "../types/transactionType";
 
 interface MonthlyDataTypes {
     trans:TransactionType[],
@@ -9,18 +9,30 @@ interface MonthlyDataTypes {
 
 export const useUserData = () => {
 const trans = useAppSelector((state) => state.transaction.transactions);
+const incomeTrans = useAppSelector((state) => state.incomeTransaction.incomeTransactions)
 const now = new Date();
 const normalizedCurrentDate = new Date(now.getFullYear(), now.getMonth(), 1);
 const currMonthData = getMonthlyData({trans,month:normalizedCurrentDate.getMonth()})
 const prevMonthData = getMonthlyData({trans,month:normalizedCurrentDate.getMonth() - 1});
+const currMonthIncome = getMonthlyIncome({transactions:incomeTrans})
 
 const currentLabels = Object.keys(currMonthData);
 const currentMonthAmounts = currentLabels.map((label) => currMonthData[label] ?? 0);
+const currentMonthExpense = getMonthlyExpense({trans:trans,month:normalizedCurrentDate.getMonth()})
+
 
 const trendLabels = Array.from(new Set([
     ...Object.keys(currMonthData),
     ...Object.keys(prevMonthData),
 ]));
+
+const incomeVsExpenseLabels = Array.from(new Set([
+    ...Object.keys(currMonthIncome),
+    ...Object.keys(currentMonthExpense)
+]))
+
+const currMonthIncomeAmounts = incomeVsExpenseLabels.map((label) => currMonthIncome[label] ?? 0)
+const currMonthExpenseAmounts = incomeVsExpenseLabels.map((label) => currentMonthExpense[label] ?? 0)
 
 const currMonthAmounts = trendLabels.map((label) => currMonthData[label] ?? 0);
 const prevMonthAmounts = trendLabels.map((label) => prevMonthData[label] ?? 0);
@@ -74,7 +86,27 @@ const lineData:ChartData<"line"> = {
     ]
 }
 
-return {pieData,barData,lineData}
+const analysisData:ChartData<"line"> = {
+    labels:incomeVsExpenseLabels,
+    datasets:[
+        {
+            label:"This Month income Analysis",
+            data:currMonthIncomeAmounts,
+            borderColor: "green",
+            borderWidth:2,
+            backgroundColor:"red"
+        },
+        {
+            label:"This Month expense Analysis",
+            data:currMonthExpenseAmounts,
+            borderColor: "red",
+            borderWidth:2,
+            backgroundColor:"rgb(75,192,192)"
+        },
+    ]
+}
+
+return {pieData,barData,lineData,analysisData}
 }
 
 function getMonthlyData({trans,month}:MonthlyDataTypes){
@@ -97,4 +129,53 @@ function getMonthlyData({trans,month}:MonthlyDataTypes){
             }
             return acc;
         }, {});
+}
+
+function getMonthlyExpense({trans,month}:MonthlyDataTypes){
+     const now = new Date();
+    const targetDate = new Date(now.getFullYear(), month, 1);
+    const start = 1;
+    const end = targetDate.getFullYear()
+    return trans.filter((t) => {
+        const date = new Date(t.date)
+      return  date.getMonth() >= start && date.getFullYear() <= end;
+    })
+        .reduce<Record<string, number>>((acc, curr) => {
+            if(!acc[getMonthName(curr.date)]){
+                acc[getMonthName(curr.date)] = (acc[getMonthName(curr.date)] ?? 0) + Number(curr.amount);
+            }
+            else{
+                acc[getMonthName(curr.date)] += Number(curr.amount)
+            }
+            return acc;
+        }, {});
+}
+interface MonthlyIncomeType {
+    transactions:IncomeType[];
+}
+function getMonthlyIncome({transactions}:MonthlyIncomeType){
+   const now = new Date();
+    const targetDate = new Date(now.getFullYear(), now.getMonth(), 1);
+    const start = 1;
+    const end = targetDate.getFullYear()
+  return transactions.filter((t) => {
+    const date = new Date(t.date)
+   return date.getMonth() >= start && date.getFullYear() <= end
+  })
+    .reduce<Record<string,number>>((acc,curr) => {
+     
+    if(!acc[getMonthName(curr.date)]){
+     acc[getMonthName(curr.date)] = (acc[getMonthName(curr.date)] ?? 0) + Number(curr.amount)
+    }
+    else{
+        acc[getMonthName(curr.date)] += Number(curr.amount)
+    }
+    return acc;
+ },{})
+}
+
+function getMonthName(dateString:string){
+ return new Date(dateString).toLocaleDateString("en-US",{
+    month:"short"
+ })
 }
