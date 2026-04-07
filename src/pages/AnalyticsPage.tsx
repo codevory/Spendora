d3840fa5ab92b35cd1c23422dfd32cc076b76fdf
codevory <shahijahan.codes@gmail.com>
2026-04-07 19:15:46 +0530

    content added

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -1,16 +1,42 @@
+import DistributionGraph from '../charts/DistributionGraph';
+import TrendGraph from '../charts/TrendGraph';
 import Layout from '../components/Layout'
+import MonthlyInsights from '../components/MonthlyInsights';
+import RecentTransactions from '../components/RecentTransactions';
+import { useUserData } from '../Hooks/useUserData';
 
 interface AnalyticsPropsType {
     onToggle:() => void;
     isOpen:boolean;
 }
 const AnalyticsPage = ({onToggle,isOpen}:AnalyticsPropsType) => {
+
+  const {analysisData} = useUserData()
   return (
-    <div>
+    <div className='bg-main'>
         <Layout onToggle={onToggle} isOpen={isOpen}>
-     <h1>
-        this is analytics page
-     </h1>
+       <div className='flex flex-col gap-1'>
+        <div className='flex gap-2'>
+
+          <div className='h-100 overflow-y-scroll w-1/2'>
+            <RecentTransactions />
+          </div>
+      
+          <div className='w-2/3'>
+            <TrendGraph data={analysisData} />
+          </div>
+
+        </div>
+    
+         <div className='flex w-full bg-yellow-950'>
+          <div className='h-full w-auto'>
+            <DistributionGraph />
+          </div>
+          <div className='w-2/3 bg-green-500 '>
+          <MonthlyInsights />
+          </div>
+         </div>
+       </div>
 
         </Layout>
       
