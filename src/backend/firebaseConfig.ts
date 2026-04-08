4b8bdf3dfdd3fa11eabd37a0a511e378cc99d872
codevory <shahijahan.codes@gmail.com>
2026-04-08 20:12:12 +0530

    firebase configurations

---
+++ src/backend/firebaseConfig.ts
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth/web-extension";
+
+const Api_key = import.meta.env.VITE_FIREBASE_API_KEY;
+const Auth_domain = import.meta.env.VITE_Auth_Domain
+const Project_Id = import.meta.env.VITE_Project_Id
+const Storage_bucket = import.meta.env.VITE_Storage_Bucket
+const messagingSender_Id = import.meta.env.VITE_messagingSender_Id
+const app_Id = import.meta.env.VITE_app_Id
+const measurement_Id = import.meta.env.VITE_measurement_Id
+
+const firebaseConfig = {
+  apiKey: Api_key,
+  authDomain: Auth_domain,
+  projectId: Project_Id,
+  storageBucket: Storage_bucket,
+  messagingSenderId: messagingSender_Id,
+  appId: app_Id,
+  measurementId: measurement_Id
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app)
+
