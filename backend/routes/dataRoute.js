52b138985e296c7cd325c983ae7249350926a6a6
codevory <shahijahan.codes@gmail.com>
2026-05-20 19:33:09 +0530

    added delete catgory route

--- backend/routes/dataRoute.js
+++ backend/routes/dataRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {
   addNewCategory,
+  deleteCategory,
   getCategories,
+  renameCategory,
 } from "../controllers/categoryController.js";
 import { requireAuth } from "../middleware/requireAuth.js";
 
 export const dataRoute = express.Router();
 dataRoute.get("/categories", requireAuth, getCategories);
 dataRoute.post("/addNewCategory", requireAuth, addNewCategory);
+dataRoute.post("/renameCategory", requireAuth, renameCategory);
+dataRoute.delete("/deleteCategory", requireAuth, deleteCategory);
