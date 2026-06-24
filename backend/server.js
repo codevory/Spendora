691f9ddffcac016169432b5e64eaa70ea7d9a2e9
codevory <shahijahan.codes@gmail.com>
2026-06-24 21:29:01 +0530

    formatting fix

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,11 @@ const dbPool = await getDBConnection();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:2122"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:2122",
+      "http://localhost:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
