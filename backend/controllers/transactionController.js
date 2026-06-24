f95bbb127fb0f19a5826fc92664f9cb7caea6fc1
codevory <shahijahan.codes@gmail.com>
2026-06-24 21:30:00 +0530

    packages

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,26 +1,37 @@
 import { getDBConnection } from "../db/getBDConnection.js";
-import validator from "validator";
+
 let getExpenseCount = 0;
 
 export async function getExpense(req, res) {
   if (!req.session.userId) {
     return res.status(400).json({ eror: "user not authenticated" });
   }
+
   const db = await getDBConnection();
+  getExpenseCount += 1;
 
   try {
     const expensesResult = await db.query(
-      "SELECT * FROM userexpense WHERE user_id = $1",
+      `SELECT 
+        e.id,
+        e.amount,
+        e.paid_to AS paidTo,
+        e.paid_on AS date,
+        e.transaction_id AS transactionId,
+        c.name AS categoryName,
+        e.category_id AS categoryId
+        FROM userexpense e
+        LEFT JOIN expensecategories c ON e.category_id = c.id
+        WHERE e.user_id = $1`,
       [req.session.userId],
     );
 
-    const expenses = expensesResult.rows;
-    return res.status(200).json({ expenses });
+    console.log("getExpense : ", getExpenseCount);
+    return res.status(200).json({ expenses: expensesResult.rows });
   } catch (err) {
     console.error("Error getting expense : ", err.message);
     return res.status(500).json({ error: "Internal server Error" });
   }
-  return () => clearTimeout(timer);
 }
 
 export async function addExpense(req, res) {
@@ -37,7 +48,7 @@ export async function addExpense(req, res) {
 
   try {
     await db.query(
-      "INSERT INTO userexpense (user_id,amount,paid_to,paid_on,category,transaction_id) VALUES($1,$2,$3,$4,$5,$6)",
+      "INSERT INTO userexpense (user_id,amount,paid_to,paid_on,category_id,transaction_id) VALUES($1,$2,$3,$4,$5,$6)",
       [
         req.session.userId,
         transactionData.amount,
@@ -48,7 +59,7 @@ export async function addExpense(req, res) {
       ],
     );
 
-    return res.status(201).json({ message: "Expense Added successfully🎉" });
+    return res.status(201).json({ transactionData });
   } catch (err) {
     console.error("Failed to add expense ", err.message);
     return res.status(500).json({ error: "Internal server error" });
@@ -57,10 +68,11 @@ export async function addExpense(req, res) {
 
 export async function addIncome(req, res) {
   const { incomeData } = req.body;
-  if (typeof incomeData.amount !== "number" || incomeData.amount <= 0) {
+
   if (typeof incomeData.amount !== "number" || incomeData.amount <= 0) {
     return res.status(400).json({ error: "Invalid amount" });
   }
+
   const db = await getDBConnection();
 
   try {
@@ -75,23 +87,26 @@ export async function addIncome(req, res) {
       ],
     );
 
-    return res.status(201).json({ response: "Income added successfully🎉" });
+    console.log(incomeData, " added successfully🎉");
+    return res.status(201).json({ incomeData });
   } catch (err) {
     console.error("Failed to add income : ", err.message);
-    return res.status(500).json({ error: `Internal Server Error` });
+    return res
+      .status(500)
+      .json({ error: `Failed to add income ${err.message}` });
   }
 }
 
 export async function getIncome(req, res) {
   const db = await getDBConnection();
+
   try {
     const incomeResult = await db.query(
-      "SELECT * FROM userincome WHERE user_id = $1",
+      "SELECT amount,source,recieved_on as date,transaction_id as transactionId,created_at as createdAt FROM userincome WHERE user_id = $1",
       [req.session.userId],
     );
 
-    const data = incomeResult.rows;
-    return res.status(200).json({ data });
+    return res.status(200).json({ data: incomeResult.rows });
   } catch (err) {
     console.error("Failed to get income : ", err.message);
     return res.status(500).json({ error: ` Internal Server Error ` });
