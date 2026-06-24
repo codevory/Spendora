213ef0c33b950e419db5b5c9b2990916a8dd010e
codevory <shahijahan.codes@gmail.com>
2026-06-24 20:17:35 +0530

    configure database connection

--- backend/db/getBDConnection.js
+++ backend/db/getBDConnection.js
@@ -1,12 +1,16 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import path from "node:path";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pg;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 export async function getDBConnection() {
-  const dbPath = path.join("database.db");
-
-  return open({
-    filename: dbPath,
-    driver: sqlite3.Database,
-  });
+  return pool;
 }
