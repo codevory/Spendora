18458561274d0372d10280156369087e073d099f
codevory <shahijahan.codes@gmail.com>
2026-06-29 18:36:11 +0530

    Fix :Fixes db connection failure

--- backend/db/getBDConnection.js
+++ backend/db/getBDConnection.js
@@ -6,13 +6,8 @@ const { Pool } = pkg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
   },
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
 });
 
 export async function getDBConnection() {
