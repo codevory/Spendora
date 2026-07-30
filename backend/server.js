83da7af4e3c184173257e3f4b4027400290c83a1
codevory <shahijahan.codes@gmail.com>
2026-07-30 15:29:20 +0530

    refactor(api) : standardise REST endpoints to v1

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,14 @@ import { meRouter } from "./routes/meRouter.js";
 import { authRouter } from "./routes/auth.js";
 import path from "node:path";
 import { transactionRoute } from "./routes/transactionRoute.js";
-import { dataRoute } from "./routes/dataRoute.js";
+import { categoryRoute } from "./routes/categoryRoute.js";
 import { fileURLToPath } from "node:url";
 import { serverHealthRoute } from "./routes/serverHealthRoute.js";
 import { csrfProtection } from "./middleware/csrfProtection.js";
 import cookieParser from "cookie-parser";
+import { expenseRoute } from "./routes/expenseRoute.js";
+import { incomeRoute } from "./routes/incomeRoute.js";
+import { getDataRateLimiter } from "./helpers/rateLimiters.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,24 +32,25 @@ app.set("trust proxy", 1);
 const PORT = process.env.PORT || 2122;
 const PostgresStore = pgSession(session);
 const dbPool = await getDBConnection();
-
 // 2. CORS MIDDLEWARE
-const allowedOriginsProd = ["https://spendora-khaki.vercel.app"];
-const allowedOriginsDev = [
+const allowed_origins_prod = ["https://spendora-khaki.vercel.app"];
+const allowed_origins_dev = [
   "http://localhost:5173",
   "http://localhost:2122",
   "http://localhost:3000",
 ];
 
+const allowed_origns = is_Production
+  ? allowed_origins_prod
+  : allowed_origins_dev;
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (is_Production && allowedOriginsProd.includes(origin)) {
-        callback(null, true);
-      } else if (!is_Production && allowedOriginsDev.includes(origin)) {
+    origin: (origins, callback) => {
+      if (allowed_origns.includes(origins)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by cors"));
+        callback(null, false);
       }
     },
     credentials: true,
@@ -66,7 +70,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 4. SESSION MIDDLEWARE (Must come before CSRF protection)
 app.use(
   session({
     store: new PostgresStore({
@@ -85,11 +88,13 @@ app.use(
   }),
 );
 
-app.use("/api/auth/me", meRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/transaction", transactionRoute);
-app.use("/api/data", dataRoute);
-app.use("/api/status", serverHealthRoute);
+app.use("/api/v1/auth/me", meRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/transaction", transactionRoute);
+app.use("/api/v1/transaction/expenses", expenseRoute);
+app.use("/api/v1/transaction/incomes", incomeRoute);
+app.use("/api/v1/categories", categoryRoute);
+app.use("/api/v1/status", serverHealthRoute);
 
 app.listen(PORT, () => {
   try {
